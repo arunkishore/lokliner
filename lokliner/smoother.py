753e0c725970249ee8cf6d8ceef2f30900ee197e
arunkishore <r.arunkishore@gmail.com>
2017-11-17 10:48:03 +0100

    Update smoother.py

--- lokliner/smoother.py
+++ lokliner/smoother.py
@@ -22,7 +22,7 @@ class Manipulation:
     def apply_rdp(self,
                   locations,
                   time_array,
-                  epsilon=0.5,
+                  epsilon=0.005,
                   algo='iter'):
 
         if len(locations) < 1:
